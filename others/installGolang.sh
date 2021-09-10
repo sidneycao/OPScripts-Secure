9618caef4aff82f74ebf31abb3ec04e0c1cfb7f5
Simoncao <13671707129@126.com>
2021-09-10 16:44:54 +0800

    [feat]add installGolang

--- others/installGolang.sh
+++ others/installGolang.sh
@@ -3,11 +3,11 @@
 # 一键安装最新版的Golang
 # 
 
-url=`curl -s https://golang.org/dl/ | grep downloadBox | grep linux | awk -F'=\"' '{print $3}' | awk -F'\">' '{print $1}'`
+filename=`curl -s https://golang.org/dl/ | grep downloadBox | grep linux | awk -F'/dl/' '{print $2}' | awk -F'\">' '{print $1}'`
 
-wget -P /root/ https://golang.org${url}
+wget -P /root/ https://golang.org/dl/${filename}
 
-tar -zxvf /root/linux-amd64.tar.gz -C  /usr/local/
+tar -zxvf /root/${filename} -C  /usr/local/
 
 echo "PATH=$PATH:$HOME/bin:/usr/local/go/bin" >> /etc/profile
 
