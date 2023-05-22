77a2ec0d1fdd9f81ec5b72d3cec9e4c10d4f7ef7
Simoncao <13671707129@126.com>
2023-05-22 17:31:27 +0800

    [fix]/srv/ > /etc/

--- others/installNodeExporter.sh
+++ others/installNodeExporter.sh
@@ -8,16 +8,16 @@ cd /etc/
 
 mv node_exporter-1.5.0.linux-amd64 node_exporter
 
-echo -e "#Prometheus Node Exporter Upstart script\n
-[Unit]\n
-Description=Node Exporter\n
-Wants=network-online.target\n
-After=network-online.target\n
-\n
-[Service]\n
-ExecStart=/srv/node_exporter/node_exporter\n
-\n
-[Install]\n
+echo -e "#Prometheus Node Exporter Upstart script
+[Unit]
+Description=Node Exporter
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+ExecStart=/etc/node_exporter/node_exporter
+
+[Install]
 WantedBy=default.target" > /usr/lib/systemd/system/node_exporter.service
 
 systemctl daemon-reload
