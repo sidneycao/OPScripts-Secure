9541777eff1d93e703d2d2d72c4571b36e8a8aaf
Simoncao <13671707129@126.com>
2023-05-23 11:09:44 +0800

    [feat]blackbox exporter

---
+++ others/installBlackBoxExporter.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+wget https://github.com/prometheus/blackbox_exporter/releases/download/v0.24.0/blackbox_exporter-0.24.0.linux-amd64.tar.gz
+
+tar blackbox_exporter-0.24.0.linux-amd64.tar.gz -C /etc/
+
+cd /etc/
+
+mv blackbox_exporter-0.24.0.linux-amd64.tar.gz blackbox_exporter
+
+echo -e "#Prometheus Blackbox Exporter Upstart script
+[Unit]
+Description=Blackbox Exporter
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+ExecStart=/etc/blackbox_exporter/blackbox_exporter
+
+[Install]
+WantedBy=default.target" > /usr/lib/systemd/system/blackbox_exporter.service
+
+systemctl daemon-reload
+systemctl enable blackbox_exporter
+systemctl start blackbox_exporter
+systemctl status blackbox_exporter
